764402de438f83dd432fea793243f0f012a30706
Zarina92c <zkurtbedinova@gmail.com>
2020-08-01 12:07:33 -0400

    Add to HelloB20.java

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -1,5 +1,6 @@
 public class HelloB20 {
     public static void main(String[] args) {
-        System.out.println("It is a pleasure to be here");
+        System.out.println("B 20 is always happy");
+
     }
 }
